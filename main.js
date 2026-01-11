9b0ca0782b79ca5dc9468b0f2cded2872e88ad87
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2026-01-11 11:58:50 -0600

    Add Prettier and format code

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const addXpButton = document.getElementById("addXp");
 let level = 1;
 let xp = 0;
 addXpButton.addEventListener("click", () => {
-  xp++;
-  if (xp >= level) {
-    xp -= level;
-    level++;
-  }
+    xp++;
+    if (xp >= level) {
+        xp -= level;
+        level++;
+    }
 });
