f6e3a5d7de4cba9991b196f562b9667ed15e72cc
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2026-01-11 11:48:56 -0600

    Add JavaScript code

---
+++ main.js
@@ -0,0 +1,12 @@
+const currentLevelText = document.getElementById('currentLevel');
+const xpProgressText = document.getElementById('xpProgress');
+const addXpButton = document.getElementById('addXp');
+let level = 1;
+let xp = 0;
+addXpButton.addEventListener('click', () => {
+    xp++;
+    if (xp >= level) {
+        xp -= level;
+        level++;
+    }
+})
