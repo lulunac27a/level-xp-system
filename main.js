095867c3356c85165bdee0c428b04b43763a8f63
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2026-01-11 18:56:48 -0600

    Add comments

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-const currentLevelText = document.getElementById("currentLevel");
-const xpProgressText = document.getElementById("xpProgress");
-const addXpButton = document.getElementById("addXp");
-let level = 1;
-let xp = 0;
+const currentLevelText = document.getElementById("currentLevel"); //current level text
+const xpProgressText = document.getElementById("xpProgress"); //xp progress text
+const addXpButton = document.getElementById("addXp"); //add xp button
+let level = 1; //set default level
+let xp = 0; //set default xp
 addXpButton.addEventListener("click", () => {
-    xp++;
+    //when add xp button is clicked
+    xp++; //increase xp
     if (xp >= level) {
-        xp -= level;
-        level++;
+        //check if xp is greater than or equal to level
+        xp -= level; //subtract level from xp
+        level++; //increase level
     }
-    currentLevelText.textContent = level;
-    xpProgressText.textContent = `${xp}/${level}`;
+    currentLevelText.textContent = level; //update current level text
+    xpProgressText.textContent = `${xp}/${level}`; //update xp progress text
 });
