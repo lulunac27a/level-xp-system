c51b07732ced6c3b5c1cec0d1b11c1a070d811cd
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2026-01-14 10:52:54 -0600

    Add method to reset XP values

--- html-app/main.js
+++ html-app/main.js
@@ -1,6 +1,7 @@
 const currentLevelText = document.getElementById("currentLevel"); //current level text
 const xpProgressText = document.getElementById("xpProgress"); //xp progress text
 const addXpButton = document.getElementById("addXp"); //add xp button
+const resetXpButton = document.getElementById("resetXp"); //reset xp button
 let level = 1; //set default level
 let xp = 0; //set default xp
 addXpButton.addEventListener("click", () => {
@@ -13,4 +14,9 @@ addXpButton.addEventListener("click", () => {
     }
     currentLevelText.textContent = level; //update current level text
     xpProgressText.textContent = `${xp}/${level}`; //update xp progress text
+});
+resetXpButton.addEventListener("click", () => {
+    //when reset xp button is clicked
+    level = 1; //reset level to 1
+    xp = 0; //reset xp to 0
 });
